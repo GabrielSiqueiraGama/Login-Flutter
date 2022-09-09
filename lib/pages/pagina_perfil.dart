3fe79bc23bec8c7f50abc53623cf56fbdc4d9d2a
Gabriel Siqueira Gama <Siqueiragamagabriel@gmail.com>
2022-09-09 11:21:36 -0300

    adicionando pagina do usuario

--- lib/pages/pagina_perfil.dart
+++ lib/pages/pagina_perfil.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:perfil_user/pages/login_page.dart';
+import 'package:perfil_user/pages/tela_abertura.dart';
+import 'package:perfil_user/pages/widgets/header_widget.dart';
 
 class PaginaPerfil extends StatefulWidget {
   const PaginaPerfil({Key? key}) : super(key: key);
@@ -13,57 +16,242 @@ class _PaginaPerfil extends State<PaginaPerfil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Pagina de perfil.',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        elevation: 0.5,
-        iconTheme: const IconThemeData(color: Colors.white),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: const Text(
+            'Pagina de perfil.',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Colors.white),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
               gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                Theme.of(context).primaryColor,
-                Theme.of(context).primaryColor.withOpacity(0.4)
-              ])),
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  Theme.of(context).primaryColor.withOpacity(0.2),
+                  Theme.of(context).primaryColor.withOpacity(0.5),
+                ],
+              ),
+            ),
+          ),
         ),
-        actions: [
-          Container(
-            margin: const EdgeInsets.only(top: 16, right: 16),
-            child: Stack(
-              children: <Widget>[
-                const Icon(Icons.notifications),
-                Positioned(
-                    child: Container(
-                  padding: const EdgeInsets.all(1),
+        drawer: Drawer(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: const [0.0, 1.0],
+                colors: [
+                  Theme.of(context).primaryColor.withOpacity(0.2),
+                  Theme.of(context).primaryColor.withOpacity(0.3),
+                ],
+              ),
+            ),
+            child: ListView(
+              children: [
+                DrawerHeader(
                   decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  constraints: const BoxConstraints(
-                    minWidth: 12,
-                    minHeight: 12,
-                  ),
-                  child: const Text(
-                    '1',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 8,
+                    color: Theme.of(context).primaryColor,
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: const [0.0, 1.0],
+                      colors: [
+                        Theme.of(context).primaryColor.withOpacity(0.9),
+                        Theme.of(context).primaryColor.withOpacity(0.5),
+                      ],
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                ))
+                  child: Container(
+                    alignment: Alignment.bottomLeft,
+                    child: const Text(
+                      'Texto',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                ListTile(
+                    leading: Icon(
+                      Icons.accessibility,
+                      size: 24,
+                      color: Colors.purple.shade400,
+                    ),
+                    title: Text(
+                      "Tela de abertura",
+                      style: TextStyle(
+                          fontSize: 17, color: Colors.purple.shade400),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const TelaAbertura(
+                            title: "Tela de abertura.",
+                          ),
+                        ),
+                      );
+                    }),
+                ListTile(
+                    leading: Icon(
+                      Icons.accessibility,
+                      size: 24,
+                      color: Colors.purple.shade400,
+                    ),
+                    title: Text(
+                      "Tela de login",
+                      style: TextStyle(
+                          fontSize: 17, color: Colors.purple.shade400),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    }),
               ],
             ),
-          )
-        ],
-      ),
-      drawer: Drawer(
-        child: Container(),
-      ),
-    );
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              const SizedBox(
+                height: 100,
+                child: HeaderWidget(100, false, Icons.house_rounded),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(width: 5, color: Colors.white),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              offset: Offset(5, 2))
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.person,
+                        size: 80,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Gabriel Siqueira Gama",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Criador do aplicativo.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(children: <Widget>[
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                          alignment: Alignment.topLeft,
+                          child: const Text(
+                            "Informações do usuario",
+                            style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.all(15),
+                            child: Column(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ...ListTile.divideTiles(
+                                        color: Colors.grey,
+                                        tiles: [
+                                          const ListTile(
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    horizontal: 12,
+                                                    vertical: 4),
+                                            leading: Icon(Icons.my_location),
+                                            title: Text("Cidade"),
+                                            subtitle: Text("Balsas-MA."),
+                                          ),
+                                          const ListTile(
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    horizontal: 12,
+                                                    vertical: 4),
+                                            leading: Icon(Icons.email),
+                                            title: Text("E-mail"),
+                                            subtitle: Text(
+                                                "siqueiragamaGabriel@gmail.com"),
+                                          ),
+                                          const ListTile(
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    horizontal: 12,
+                                                    vertical: 4),
+                                            leading: Icon(Icons.phone),
+                                            title: Text("Numero de contato"),
+                                            subtitle:
+                                                Text("+55 (99) 9 88206473"),
+                                          ),
+                                          const ListTile(
+                                            contentPadding:
+                                                EdgeInsets.symmetric(
+                                                    horizontal: 12,
+                                                    vertical: 4),
+                                            leading: Icon(Icons.person),
+                                            title: Text("Sobre mim"),
+                                            subtitle: Text(
+                                                "Objetivo de me tornar um programador flutter."),
+                                          ),
+                                        ])
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
